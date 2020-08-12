863faa880643b22a9347f8c489a99876046fd690
jd <duverseaujohnny21@gmail.com>
2020-08-12 19:04:55 -0400

    refactor index.html, scripts.js
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- scripts.js
+++ scripts.js
@@ -6,17 +6,27 @@ $(document).ready(function() {
      let inputtedDate = $("#date").val();
      let inputtedStart = $("#start").val();
     let inputtedEnd = $("#end").val();
-   
-$(".name").prepend("<li>" + inputtedName + "</li>")
- $(".description").prepend("<li>" + inputtedDescription + "</li>")
- $(".date").prepend("<li>" +inputtedDate + "</li>")
- $(".start").prepend("<li>" + inputtedStart + "</li>")
- $(".end").prepend("<li>" + inputtedEnd  + "</li>")
-// $(".date").text(inputtedDate)
-// $(".start").text(inputtedStart)
-// $(".end").text(inputtedEnd)
-// $("p. description").hide()
 
+    if (inputtedName + inputtedDescription + inputtedDate + inputtedStart + inputtedEnd) {
+      $(".name").prepend("<li>" + inputtedName + "</li>")
+    }
+  
     $("#story").show();
   })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
